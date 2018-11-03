a5a35304a2676fdad105a4599d0ea0090022a8db
cforno12 <cforno1@binghamton.edu>
2018-11-03 00:13:36 -0400

    handled init messages

--- branch.py
+++ branch.py
@@ -4,32 +4,48 @@ sys.path.append('/home/vchaska1/protobuf/protobuf-3.5.1/python')
 
 import bank_pb2
 
+def init_valid(name, ip, port, m):
+    branch = m.Branch()
 
-def populate_branch(branch):
-    branch.name = sys.argv[1]
-    branch.ip = socket.gethostbyname(socket.gethostname())
-    branch.port = sys.argv[2]
-
-    return branch
-    
+    if branch.name == name and branch.ip == ip  and branch.port == port:
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
+    name = sys.argv[1]
+    ip = socket.gethostbyname(socket.gethostname())
+    port = sys.argv[2]
 
-    message = bank_pb2.BranchMessage()
-    init_branch = message.init_branch
-    transfer = message.transfer
-    init_snapshot = message.init_snapshot
-    retrieve_snapshot = message.retrieve_snapshot
-    return_snapshot = message.return_snapshot
+    balance  = 0
 
-    branch = populate_branch(message.init_branch.Branch())
-
-    init_branch.all_branches.extend([branch])
+    time_interval = sys.argv[3]
     
-
     if interval > 1000:
         #output logs of Transfer messages
         pass
+
+    branches = []
+
+    # once we recieve a message we check if the init message is populated
+    # if it isn't then we assume it is a transfer message
+    message = bank_pb2.BranchMessage()
+    
+    if message.init_branch.Branch().name == '':
+        # it is a transfer message
+        pass
+    else:
+        #it is an init message
+        message = message.init_branch
+        if init_valid(name, ip, port, message):
+            balance = message.balance
+            for b in message.all_branches:
+                branches.extend(b)
+        else:
+            print("ERROR: Init message was sent to the wrong branch.")
+
+    
+
 
     
 
