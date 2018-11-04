2c05506ff0afb73491374d52685ecb349738ac46
cforno12 <cforno1@binghamton.edu>
2018-11-04 00:02:13 -0400

    managed to send messages through sockets

--- branch.py
+++ branch.py
@@ -4,6 +4,48 @@ sys.path.append('/home/vchaska1/protobuf/protobuf-3.5.1/python')
 
 import bank_pb2
 
+class Branch:
+
+    def __init__(self, name, port, time, sock):
+        self.name = name
+        self.port = port
+        self.ip = socket.gethostbyname(socket.gethostname())
+        self.own_branch = bank_pb2.BranchMessage()
+        self.time_interval = time
+        self.balance = 0
+        self.branches = []
+        self.socket = sock
+
+
+    def handle_message(self, client_socket, client_add):
+        while True:
+            msg = client_socket.recv(1024)
+            if not msg:
+                break
+            
+            self.own_branch.ParseFromString(msg)
+            print self.own_branch
+            sys.exit(0)
+                
+
+        
+
+    def run(self):
+        self.socket.bind((self.ip, self.port))
+        self.socket.listen(5)
+
+        while True:
+            try:
+                print "Branch on ", self.ip, "on port", self.port
+                client_socket, client_add = self.socket.accept()
+                self.handle_message(client_socket, client_add)
+                
+            except KeyboardInterrupt:
+                self.socket.close()
+                print("Closing socket...")
+                sys.exit(0)
+
+                
 def init_valid(name, ip, port, m):
     branch = m.Branch()
 
@@ -13,19 +55,22 @@ def init_valid(name, ip, port, m):
         return False
 
 if __name__ == "__main__":
+
+    if(len(sys.argv) != 4):
+        print("Usage: python branch.py <branch_name> <port> <time_interval>")
+        sys.exit(1)
+
     name = sys.argv[1]
     ip = socket.gethostbyname(socket.gethostname())
-    port = sys.argv[2]
-
-    balance  = 0
-
+    port = int(sys.argv[2])
     time_interval = sys.argv[3]
     
-    if interval > 1000:
-        #output logs of Transfer messages
-        pass
 
-    branches = []
+    mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    current_branch = Branch(name, port, time_interval, mysocket)
+    current_branch.run()
+
+
 
     # once we recieve a message we check if the init message is populated
     # if it isn't then we assume it is a transfer message
