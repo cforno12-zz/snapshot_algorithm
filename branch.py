e200cd384493fcba8edc673efdc400b5fc45ef21
Kyle <kfranke1@binghamton.edu>
2018-11-11 00:31:47 -0500

    starting lamport

--- branch.py
+++ branch.py
@@ -15,12 +15,11 @@ class Snapshot:
         self.balance = balance
         self.channels = {} # key: name of source; value: money transfered
         # key: name of source; bool tells us if the channel is active or not
-        # all set as false by default, set to true when we recieve a marker message from the source a second time
+        # all set as false by default, set to true when we receive a marker message from the source a second time
         self.active_channels = {}
         self.retrieved = False
 
 class Branch:
-
     def __init__(self, name, port, time, sock):
         self.name = name
         self.port = port
@@ -32,50 +31,58 @@ class Branch:
         self.socket = sock # server socket
         self.log_bool = False
         self.snapshots = {} #key: snap_id; value: snap_obj
+        self.controller = None
 
-    def init_msg(self, msg):
+    def init_msg(self, msg, client_add):
         self.balance = msg.balance
         self.branches = msg.all_branches
-        thread.start_new_thread(self.send_transfer_msgs())
+        self.controller = client_add
+        thread.start_new_thread(self.send_transfer_msgs, ())
 
-    def recieve_transfer_msg(self, msg):
-        print("Trying to receive")
-        print(msg)
+    def receive_transfer_msg(self, msg):
         if msg.dst_branch == self.name:
             for ss_id, ss_obj in self.snapshots:
                 if ss_obj.retrieved == False and ss_obj.active_channels[msg.src_branch] == False:
-                    print "we are setting the channel's state"
                     ss_obj.channels[msg.src_branch] += msg.money
             self.balance_lock.acquire()
+            print("Just received " + str(msg.money) + " from branch " + str(msg.src_branch))
             self.balance += msg.money
+            print("New balance: " + str(self.balance))
             self.balance_lock.release()
         else:
             print("Recieved wrong transfer message")
             
     def send_transfer_msgs(self):
         while True:
-            for name, socket in self.branch_sockets.iteritems():
-                self.balance_lock.acquire()
-                send_balance = int(round(random.randint(int(self.balance*0.01), int(self.balance*0.05))))
-                # check if we have enough money
-                if (self.balance - send_balance) < 0:
-                    print "Cannot send more money."
-                    self.balance_lock.release()
-                    continue
-                self.balance -= send_balance
+            random_branch = self.branches[random.randint(0, len(self.branches)-1)]
+            name = str(random_branch.name)
+            if name == self.name:
+                continue
+            new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            new_socket.connect((socket.gethostbyname(str(random_branch.ip)), int(random_branch.port)))
+            
+            self.balance_lock.acquire()
+            send_balance = int(round(random.randint(int(self.balance*0.01), int(self.balance*0.05))))
+            # check if we have enough money
+            if (self.balance - send_balance) < 0:
+                print "Cannot send more money."
                 self.balance_lock.release()
-                transfer_message = bank_pb2.BranchMessage()
-                transfer_message.transfer.money = send_balance
-                transfer_message.transfer.src_branch = self.name
-                transfer_message.transfer.dst_branch = name
-                if self.log_bool:
-                    print "Transferring $" + str(send_balance) + " from " + self.name + " to " + name
-                    print self.name + "'s new balance: " + str(self.balance)
-                print "Sending to " + name
-                socket.sendall(transfer_message.SerializeToString() + '\0')
-                time.sleep(2)
+                continue
+            self.balance -= send_balance
+            self.balance_lock.release()
+            transfer_message = bank_pb2.BranchMessage()
+            transfer_message.transfer.money = send_balance
+            transfer_message.transfer.src_branch = self.name
+            transfer_message.transfer.dst_branch = name
+            if self.log_bool:
+                print "Transferring $" + str(send_balance) + " from " + self.name + " to " + name
+                print self.name + "'s new balance: " + str(self.balance)
+            print "Sending to " + name
+            new_socket.sendall(transfer_message.SerializeToString() + '\0')
+            new_socket.close() 
+            time.sleep(2)
         thread.exit()
-    
+
     def init_snapshot(self, msg):
         snapshot_id = msg.snapshot_id
 
@@ -88,15 +95,21 @@ class Branch:
 
     def send_marker_msgs(self, marker_msg, snap_obj):
         #sending marker messages to all other branches
-        for name, socket in self.branch_sockets.iteritems():
+        for branch in self.branches:
+            name = str(branch.name)
+            new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            new_socket.connect((socket.gethostbyname(str(branch.ip)), int(branch.port)))
+
             snap_obj.channels[name] = 0 # no money has been transferred yet
             snap_obj.active_channels[name] = False
             marker_msg.marker.src_branch = self.name
             marker_msg.marker.dst_branch = name
-            socket.sendall(marker_msg.SerializeToString + '\0')
+            new_socket.sendall(marker_msg.SerializeToString + '\0')
+            new_socket.close()
+
         self.snapshots[snapshot_id] = snap_obj
 
-    def recieve_marker_msg(self, msg):
+    def receive_marker_msg(self, msg):
         snap_id = msg.snapshot_id
         if not snap_id in self.snapshots:
             # this is the first time the branch has seen this snapshot
@@ -111,35 +124,28 @@ class Branch:
         msg_type = msg.WhichOneof("branch_message")
 
         if msg_type == "init_branch":
-            print("initializing branch")
-            self.init_msg(msg.init_branch)
-            print("Returned from init msg")
+            self.init_msg(msg.init_branch, client_add)
         elif msg_type == "transfer":
-            self.recieve_transfer_msg(msg.transfer)
+            self.receive_transfer_msg(msg.transfer)
         elif msg_type == "init_snapshot":
             self.init_snapshot_msg(msg.init_snapshot)
         elif msg_type == "marker":
-            self.recieve_marker_msg(msg.marker)
+            self.receive_marker_msg(msg.marker)
         elif msg_type == "retrieve_snapshot":
             self.retrieve_snapshot_msg(msg.retrieve_snapshot)
-        elif msg_type == "return_snapshot":
-            self.return_snapshot_msg(msg.return_snapshot)
         else:
             print "Unrecognized message type: " + str(msg_type)
 
     def listen_for_message(self, client_socket, client_add):
-        print "listening for a message"
         msg = client_socket.recv(1024)
         if msg:
             for m in msg.split('\0')[:-1]:
                 branch_message = bank_pb2.BranchMessage()
                 branch_message.ParseFromString(m)
-                print "parsing message: " + m
                 self.parse_message(client_socket, client_add, branch_message)
 
     def run(self):
         # checking if we should log
-
         if self.time_interval >= 1000:
             self.log_bool = True
             
@@ -147,18 +153,19 @@ class Branch:
         self.socket.bind((self.ip, self.port))
         self.socket.listen(5)
         print "Branch on ", self.ip, "on port", self.port
+        #client_socket, client_add = self.socket.accept()
+        #self.listen_for_message(client_socket, client_add)
+        #time.sleep(1)
         while True:
-            print("Just reset loop in run function")
             try:
                 client_socket, client_add = self.socket.accept()
-                print("Just received a message from", client_add)
+                print(client_add)
                 thread.start_new_thread(self.listen_for_message, (client_socket, client_add))
                 #self.send_transfer_msgs() # start another thread here
             except KeyboardInterrupt:
                 self.socket.close()
                 print("Closing socket...")
                 sys.exit(0)
-        print "We are exiting our branch"
 
 if __name__ == "__main__":
 
@@ -174,3 +181,4 @@ if __name__ == "__main__":
     mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     current_branch = Branch(name, port, time_interval, mysocket)
     current_branch.run()
+    mysocket.close()
